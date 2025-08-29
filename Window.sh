87e6e68afd7f64308a92429deb33a4476b7fdbbe
Ascellayn <root.siriusb@gmail.com>
2025-08-29 21:15:03 +0200

    Fix Window capture

--- Window.sh
+++ Window.sh
@@ -11,7 +11,7 @@ mkdir -p $Folder
 cd $Folder
 
 hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' |\
-grim -g -c - "$Filename"
+grim -c -g - "$Filename"
 wl-copy < "$Filename"
 
 
